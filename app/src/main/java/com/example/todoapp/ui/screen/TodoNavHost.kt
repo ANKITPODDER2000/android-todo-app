6d600205e0ad38912ea91b7f1fea1aa8970b0eae
ankit.podder <ankitpodder0211@gmail.com>
2023-09-10 01:15:00 +0530

    add nav route for todo details page

--- app/src/main/java/com/example/todoapp/ui/screen/TodoNavHost.kt
+++ app/src/main/java/com/example/todoapp/ui/screen/TodoNavHost.kt
@@ -1,27 +1,42 @@
 package com.example.todoapp.ui.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.todoapp.utility.NavScreens
 import com.example.todoapp.viewmodel.TodoAppViewModel
 
 @Composable
-fun TodoNavHost(todoAppViewModel: TodoAppViewModel, modifier: Modifier = Modifier) {
+fun TodoNavHost(todoAppViewModel: TodoAppViewModel, navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(
-        navController = rememberNavController(),
+        navController = navController,
         startDestination = NavScreens.TODO_LIST_PAGE.name,
         modifier = modifier.fillMaxSize(1f)
     ) {
         composable(NavScreens.TODO_LIST_PAGE.name) {
-            todoAppViewModel.getAllTodo()
-            val dbState by todoAppViewModel.dbState.collectAsState()
-            TodoListScreen(dbState)
+            TodoListScreen(todoAppViewModel) { todoId ->
+                navController.navigate("${NavScreens.TODO_DETAILS_PAGE.name}/$todoId")
+            }
+        }
+        composable(
+            route = "${NavScreens.TODO_DETAILS_PAGE.name}/{id}",
+            arguments = listOf(navArgument("id") { type = NavType.StringType })
+        ) {
+            TodoDetailsScreen()
         }
     }
+}
+
+@Composable
+fun TodoDetailsScreen() {
+    Text(text = "Todo Details Screen")
 }
--- app/src/main/java/com/example/todoapp/utility/NavScreens.kt
+++ app/src/main/java/com/example/todoapp/utility/NavScreens.kt
@@ -2,5 +2,6 @@ package com.example.todoapp.utility
 
 enum class NavScreens {
     TODO_LIST_PAGE,
+    TODO_DETAILS_PAGE,
     CREATE_NEW_TODO_FORM
 }
