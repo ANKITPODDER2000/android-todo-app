1017a75ab1d8cb0f863109ca97a7fa3fc78870d1
ANKITPODDER0211 <72136836+ANKITPODDER0211@users.noreply.github.com>
2023-10-07 23:42:14 +0530

    Update TodoDao.kt

--- app/src/main/java/com/example/todoapp/db/TodoDao.kt
+++ app/src/main/java/com/example/todoapp/db/TodoDao.kt
@@ -9,21 +9,27 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
+    // This method will perform insert as well as update operation
     @Upsert
     suspend fun insertTodo(todo: Todo)
 
+    // This method will delete a particular todo
     @Query("DELETE FROM $TODO_TABLE_NAME WHERE id=:deletedId")
     suspend fun deleteTodo(deletedId: Int)
 
+    // This method will delete all records of todo table
     @Query("DELETE FROM $TODO_TABLE_NAME")
     suspend fun deleteAllTodo()
 
+    // Fetch all todos from the database
     @Query("SELECT * FROM $TODO_TABLE_NAME")
     fun getAllTodo(): Flow<List<Todo>>
 
+    // Will search for a particular todo
     @Query("SELECT * FROM $TODO_TABLE_NAME WHERE title =:searchedTodo")
     fun getSearchedTodo(searchedTodo: String): Flow<List<Todo>>
 
+    // Will search for a particular todo
     @Query("SELECT * FROM $TODO_TABLE_NAME WHERE id =:selectedId")
     fun getTodoDetail(selectedId: Int) : Flow<Todo>
 }
